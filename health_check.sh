587e358c8ebd6e930049fab7bf7393a03229e924
nknaleena101 <nknaleena101@gmail.com>
2026-04-07 11:39:32 +0530

    add configuration, parse the data and comparison logic

--- health_check.sh
+++ health_check.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 source .env
 
-curl -H "Content-Type: application/json" \
--X POST \
--d "{\"content\": \"Test from Env\"}" \
-$DISCORD_WEBHOOK_URL
+THRESHOLD=80
+WEBHOOK_URL=$DISCORD_WEBHOOK_URL
+HOSTNAME=${hostname)
+
+#parse the data
+DISK_USAGE=$(df / | awk 'NR==2 {print $5}' | tr -d '%')
+MEM_USAGE=$(free | awk '/Mem:/ {print int($3/$2 * 100)}')
+CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | awk '{print $8}' | awk -F. '{print 100 - $1}')
+
+MESSAGE=""
+
+#comparison Logic
+if [ "$DISK_USAGE" -gt "$THRESHOLD" ]; then
+	MESSAGE="${MESSAGE} ALERT: High Disk usage on $HOSTNAME: ${DISK_USAGE}% \n"
+fi
+
+if [ "$MEM_USAGE" -gt "$THRESHOLD" ]; then
+	MESSAGE="${MESSAGE} ALERT: High memory usage on $HOSTNAME: ${MEM_USAGE}% \n"
+fi
+
+if [ "$CPU_USAGE" -gt "$THRESHOLD" ]; then
+	MESSAGE="${MESSAGE} ALERT: High CPU usage on $HOSTNAME: ${CPU_USAGE}% \n"
+fi
+
+
 
